5139b7cf5e166e8a26b85612847077f538249ff7
johnsonkarasanyi <johnsonkarasanyi@gmail.com>
2019-10-16 16:32:08 +0100

    Edit Page. it is editing when you reload page. i still have to split name into[name,surname]

--- src/reducers/contactReducer.js
+++ src/reducers/contactReducer.js
@@ -30,19 +30,19 @@ export default (state=initialState,action) =>{
             )
         }
     case ADD_CONTACT:
-         return {
+        return {
              ...state,
              contacts:[action.payload,...state.contacts]
          }
-     case UPDATE_CONTACT:
-         return{
-             ...state,
-             contacts:state.contacts.map(
-                 contact=>contact.id===action.id ? 
-                 (contact=action.payload):contact
-                 )
-         } ;     
+        case UPDATE_CONTACT:
+            return{
+                ...state,
+                contacts:state.contacts.map(
+                    contact=>contact.id===action.id ? 
+                    (contact=action.payload):contact
+                    )
+            } ;     
         default:
             return state
-}
-}
+    }
+    }
